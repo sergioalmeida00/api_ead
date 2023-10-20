7cc3e886ef47d0e30f4ca886f3fa20857271ef3d
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2023-10-20 14:47:03 -0300

    REFACTOR: adicionado traits para responser de autenticação

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -4,27 +4,34 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AuthRequest;
+use App\Http\Resources\userResource;
+use App\Http\Traits\ApiResponser as TraitsApiResponser;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+    use TraitsApiResponser;
+
     public function auth(AuthRequest $request)
     {
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+            // throw ValidationException::withMessages([
+            //     'email' => ['The provided credentials are incorrect.'],
+            // ]);
+
+            return $this->error('The provided credentials are incorrect.', 422);
         }
 
         $user->tokens()->delete();
         $token = $user->createToken($request->device_name)->plainTextToken;
 
-        return response()->json([
-            'token' => $token
+        return $this->success([
+            'token' => $token,
+            'user' => new userResource($user)
         ]);
     }
 
---
+++ app/Http/Traits/ApiResponser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Traits;
+
+trait ApiResponser
+{
+    protected function success($data, $message = null, $code = 200)
+    {
+        return response()->json([
+            'status' => 'Success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function error($message = null, $code,$data = null)
+    {
+        return response()->json([
+            'status' => 'Error',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+}
